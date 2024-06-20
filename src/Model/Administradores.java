64f8d3d156ac25dafc3424f880b82817b102a8ab
LukokiMakuntimaDaniel <lukokimakuntimadaniel@gmail.com>
2024-06-20 19:08:16 +0100

    structuring the administrator model

---
+++ src/Model/Administradores.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class Administradores {
+    private int id_admin;
+    private int id_usuario;
+    private int numero_admin;
+
+    public int getId_admin() {
+        return id_admin;
+    }
+
+    public void setId_admin(int id_admin) {
+        this.id_admin = id_admin;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public int getNumero_admin() {
+        return numero_admin;
+    }
+
+    public void setNumero_admin(int numero_admin) {
+        this.numero_admin = numero_admin;
+    }
+}
